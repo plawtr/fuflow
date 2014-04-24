cf8395e3627774f2921e0aa465d209fae6ba5ca6
Andrey Krahmal <a.krahmal@plawtr.com>
2014-04-24 18:54:59 +0100

    more content and tos

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ W8::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  
   root 'application#index'
+  get 'terms' => 'application#terms'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
