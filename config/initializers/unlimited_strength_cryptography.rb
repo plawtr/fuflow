99cfb321ca7cf44de2860cbfbdbf9376148921ad
Andrey Krahmal <a.krahmal@plawtr.com>
2014-10-09 09:53:53 +0100

    text done

--- config/initializers/unlimited_strength_cryptography.rb
+++ config/initializers/unlimited_strength_cryptography.rb
@@ -1,13 +1,13 @@
 
-require 'java'
-java_import 'java.lang.ClassNotFoundException'
+# require 'java'
+# java_import 'java.lang.ClassNotFoundException'
 
-begin
-  security_class = java.lang.Class.for_name('javax.crypto.JceSecurity')
-  restricted_field = security_class.get_declared_field('isRestricted')
-  restricted_field.accessible = true
-  restricted_field.set nil, false
-rescue ClassNotFoundException => e
-  # Handle Mac Java, etc not having this configuration setting
-  $stderr.print "Java told me: #{e}n"
-end
+# begin
+#   security_class = java.lang.Class.for_name('javax.crypto.JceSecurity')
+#   restricted_field = security_class.get_declared_field('isRestricted')
+#   restricted_field.accessible = true
+#   restricted_field.set nil, false
+# rescue ClassNotFoundException => e
+#   # Handle Mac Java, etc not having this configuration setting
+#   $stderr.print "Java told me: #{e}n"
+# end
